7973bf127d02b8a76897b695d34a07fbaeb431b6
ph-1dnjs <ph.1dnjs@gmail.com>
2025-04-01 18:41:53 +0900

    feat: vite base 설정에 따른 후처리

--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -1,14 +1,17 @@
 import { createObserver } from "./createObserver";
+import { BASE_URL } from "../config.js";
 
 export const createRouter = (routes) => {
   const { subscribe, notify } = createObserver();
 
-  const getPath = () => window.location.pathname;
+  const getPath = () => window.location.pathname.replace(BASE_URL, "/");
 
-  const getTarget = () => routes[getPath()];
+  const getTarget = () => {
+    return routes[getPath()];
+  };
 
   const push = (path) => {
-    window.history.pushState(null, null, path);
+    window.history.pushState(null, null, `${BASE_URL.slice(0, -1)}${path}`);
     notify();
   };
 
